9e94dbcab6eae77aa726a240ce9aab7a8aaaabd5
narki <ivan.markovic1907@gmail.com>
2020-02-19 21:12:11 +0100

    Feature: getting information from movement

--- hdr/MovementComponent.h
+++ hdr/MovementComponent.h
@@ -16,6 +16,8 @@
 #include <SFML/Audio.hpp> 
 #include <SFML/Network.hpp> 
 
+enum movement_states { IDLE = 0, MOVING, MOVING_LEFT, MOVING_RIGHT, MOVING_UP, MOVING_DOWN };
+
 class MovementComponent
 {
 private:
@@ -39,7 +41,7 @@ public:
 	const sf::Vector2f& getVelocity() const;
 
 	//Functions
-	const bool idle() const;
+	const bool getState(const short unsigned state) const;
 	
 	void move(const float dir_x, const float dir_y, const float& dt);
 	void update(const float& dt);
