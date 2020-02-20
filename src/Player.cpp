1eacc3beff414a891bfb10fd34619b1578966e23
narki <ivan.markovic1907@gmail.com>
2020-02-20 16:14:53 +0100

    Patch: add animation in all directions

--- src/Player.cpp
+++ src/Player.cpp
@@ -24,8 +24,8 @@ Player::Player(float x, float y, sf::Texture& texture_sheet)
 	this->createMovementComponent(200.f, 15.f, 5.f);
 	this->createAnimationComponent(texture_sheet);
 
-	this->animationComponent->addAnimation("IDLE_LEFT", 10.f, 0, 0, 13, 0, 192, 192);
-	this->animationComponent->addAnimation("WALK_LEFT", 10.f, 0, 1, 11, 1, 192, 192);
+	this->animationComponent->addAnimation("IDLE", 10.f, 0, 0, 13, 0, 192, 192);
+	this->animationComponent->addAnimation("WALK", 10.f, 0, 1, 11, 1, 192, 192);
 
 		
 }
@@ -41,11 +41,23 @@ Player::~Player()
 void Player::update(const float& dt)
 {
 	this->movementComponent->update(dt);
-
+	
+	//258
 	if(this->movementComponent->getState(IDLE))
-		this->animationComponent->play("IDLE_LEFT", dt);
-	else if(this->movementComponent->getState(MOVING_LEFT))
-		this->animationComponent->play("WALK_LEFT", dt);
+		this->animationComponent->play("IDLE", dt);
+	else if(this->movementComponent->getState(MOVING_LEFT)){
+		this->sprite.setOrigin(0.f, 0.f);
+		this->sprite.setScale(1, 1);
+		this->animationComponent->play("WALK", dt);
+	}else if (this->movementComponent->getState(MOVING_RIGHT)){
+		this->sprite.setOrigin(258.f, 0.f);
+		this->sprite.setScale(-1, 1); //Flip sprite
+		this->animationComponent->play("WALK", dt);
+	}else if (this->movementComponent->getState(MOVING_UP)){
+		this->animationComponent->play("WALK", dt);
+	}else if (this->movementComponent->getState(MOVING_DOWN)){
+		this->animationComponent->play("WALK", dt);
+	}
 
 	this->hitboxComponent->update();
 }
